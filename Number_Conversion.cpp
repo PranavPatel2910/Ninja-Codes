de136a80a122fd2e4a364436697ae97bee6b87f0
Rahul Purohit <22cs071@charusat.edu.in>
2023-04-26 20:05:32 +0530

    continue

--- Number_Conversion.cpp
+++ Number_Conversion.cpp
@@ -35,53 +35,7 @@ public:
     {
         int decimalNumber = 0;
         int power = 1;
-        			
-			        // Iterate through the string in reverse order
-        for (int i = number.size() - 1; i >= 0; i--)
-        {
-            // Convert the current digit to an integer value
-            int digit;
-            if (isdigit(number[i]))
-            {
-                digit = number[i] - '0';
-            }
-            else
-            {
-                digit = number[i] - 'A' + 10;
-            }
+    }			
 
-            // Add the digit times the appropriate power of the radix to the decimal number
-            decimalNumber += digit * power;
-            power *= radix1;
-        }
-        return decimalNumber;
-    }
-
-     // Convert a decimal number to any radix
-    string toRadix(int decimalNumber)
-    {
-        string result = "";
-
-        // Keep dividing the decimal number by the radix and appending the remainders to the result
-        while (decimalNumber > 0)
-        {
-            int remainder = decimalNumber % radix2;
-           // Convert the remainder to the appropriate digit (either a number or a letter)
-            char digit;
-
-             if (remainder < 10)
-            {
-                digit = remainder + '0';
-            }
-            else
-            {
-                digit = remainder - 10 + 'A';
-            }
-        result = digit + result;
-            decimalNumber /= radix2;
-        }
-
-        return result;
-    }
 	
 };
