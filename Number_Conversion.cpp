4f6256243fb624e55317a97bbdc50b89829b9a86
Rahul Purohit <22cs071@charusat.edu.in>
2023-04-26 19:36:33 +0530

    data members

--- Number_Conversion.cpp
+++ Number_Conversion.cpp
@@ -3,4 +3,13 @@
 #include <cmath>
 
 using namespace std;
-	
+			
+class RadixConverter
+{
+    string number;
+    int radix1, radix2;
+
+public:
+    RadixConverter() {}
+    ~RadixConverter() {}
+}
