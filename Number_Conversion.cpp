68be2797db3052e22a0f77fc5efd590abfb88e64
MananPatell <22cs057.charusat.edu.in>
2023-04-26 23:49:38 +0530

    Combining both

--- Number_Conversion.cpp
+++ Number_Conversion.cpp
@@ -80,8 +80,18 @@ public:
             result = digit + result;
             decimalNumber /= radix2;
         }
-
         return result;
+    }
+     // Convert a number from radixA to radixB
+    void convertRadix()
+    {
+        // Convert the number to decimal
+        int decimalNumber = toDecimal();
+
+        // Convert the decimal number to the desired radix
+        string result = toRadix(decimalNumber);
+
+        cout << number << " in base " << radix1 << " is " << result << " in base " << radix2 << endl;
     }
 
 };
